0e3b1ebfebbea6fa4d73437dbdfe2cfd8982390f
zablackafro <zahidabdikadir12@gmail.com>
2022-04-12 12:25:05 -0500

    created my about me page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import Intro from './components/intro/Intro'
+import AboutMe from './components/aboutMe/AboutMe'
+
 function App() {
   return (
     <div className='App'>
       <Intro />
+      <AboutMe />
     </div>
   )
 }
--- src/components/aboutMe/AboutMe.jsx
+++ src/components/aboutMe/AboutMe.jsx
@@ -1,6 +1,39 @@
 import './aboutMe.css'
+import Rome from '../../assets/travel.jpg'
+import Sitting from '../../assets/profile.jpg'
+
 function AboutMe() {
-  return <div className='about'>About Me</div>
+  return (
+    <div className='a'>
+      <div className='a-left'>
+        <div className='a-card bg'></div>
+        <div className='a-card'>
+          <img src={Rome} alt='of Zahid in Rome' className='bottom a-img' />
+          <img src={Sitting} alt='of Zahid sitting' className='top a-img' />
+        </div>
+      </div>
+      <div className='a-right'>
+        <h1 className='a-tittle'>About Me</h1>
+        <p className='a-sup'>I'm a Full-stack Developer</p>
+        <p className='a-desc'>
+          I am a web developer and recent graduate of the Front End Web
+          Development Career Path bootcamp at Thinkful. I love coding, it's so
+          interesting to me. You can create so much with the skills you learn
+          and use your imagination to create some great applications. The power
+          to make your ideas come to life is such an awesome feeling. I want to
+          work on a team with great programmers and learn as much as I can from
+          them.
+        </p>
+        <br />
+        <p className='a-desc'>
+          When I'm not coding, I'm usually either reading or out with my camera
+          exploring. I also like to dance. From hip-hop to classical. I love
+          dancing and expressing myself that way. Getting lost in the music and
+          relieving some stress.
+        </p>
+      </div>
+    </div>
+  )
 }
 
 export default AboutMe
