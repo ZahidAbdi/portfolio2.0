01ef0714603ddd42eeafefe9afa5d6f884821a69
zablackafro <zahidabdikadir12@gmail.com>
2022-04-12 14:54:54 -0500

    can now display projects and click on the links to them

---
+++ src/components/projectList/ProjectList.jsx
@@ -0,0 +1,33 @@
+import './projectList.css'
+import Projects from '../projects/Projects'
+import { project } from '../../data'
+
+function ProjectList() {
+  const projectMap = project.map(item => (
+    <div className='container'>
+      <h3 className='p-name'>{item.title}</h3>
+      <Projects key={item.id} img={item.img} />
+      <p className='p-about'>{item.about}</p>
+      <br />
+      <a href={item.demo} target='_blank' rel='noreferrer' className='btn'>
+        Demo
+      </a>
+      <a href={item.link} target='_blank' rel='noreferrer' className='btn'>
+        GitHub
+      </a>
+      <hr />
+      <br />
+    </div>
+  ))
+  return (
+    <div className='pl'>
+      <div className='pl-text'>
+        <h1 className='pl-title'>My Projects</h1>
+        <p className='pl-desc'>These are the applications I've made!</p>
+      </div>
+      <div className='pl-list'>{projectMap}</div>
+    </div>
+  )
+}
+
+export default ProjectList
---
+++ src/data.js
@@ -0,0 +1,27 @@
+export const project = [
+  {
+    id: 1,
+    title: 'Github Finder',
+    about: 'App to search Github users and display their info',
+    img: require('./assets/githubfinder.PNG'),
+    link: 'https://github.com/ZahidAbdi/github-finder',
+    demo: 'https://github-finder-lilac.vercel.app/',
+  },
+  {
+    id: 2,
+    title: 'Tech Support Desk App',
+    about: 'Support ticket application built with the MERN stack',
+    img: require('./assets/techsupport.PNG'),
+    link: 'https://github.com/ZahidAbdi/support-ticket',
+    demo: 'https://zahidtechsupportdesk.herokuapp.com/',
+  },
+  {
+    id: 3,
+    title: 'House Search Marketplace',
+    about:
+      'Find and list houses for sale or for rent. This is a React / Firebase project',
+    img: require('./assets/housesearch.PNG'),
+    link: 'https://github.com/ZahidAbdi/house-search',
+    demo: 'https://house-search-market.vercel.app/',
+  },
+]
